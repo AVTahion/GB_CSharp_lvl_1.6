01a04a9d6e820b55fdd864c8075cec7a33c68641
AVTahion <avkontakt40@gmail.com>
2019-02-09 21:44:30 +0300

    Добавьте файлы проекта.

---
+++ GB_CSharp_lvl_1.6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_CSharp_lvl_1._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
